808f16c1dcc6c7a512ec4ee6ad9bbf6e8604fd5f
Emad Ghorbaninia <51790366+Emadgnia@users.noreply.github.com>
2022-10-03 13:56:24 +0200

    Revert "remove SPM dependency to Networking"

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "Networking-iOS",
+            targets: ["Networking"]),
+        .library(
             name: "Authentication",
             targets: ["Authentication"]),
     ],
@@ -19,6 +22,12 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Networking",
+            dependencies: []),
+        .testTarget(
+            name: "NetworkingTests",
+            dependencies: ["Networking"]),
         .target(
             name: "Authentication",
             dependencies: []),
