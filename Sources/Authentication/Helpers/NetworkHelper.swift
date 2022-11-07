9b42603111d49714b65379b4dc790e6db8e64117
Nicolai Øyen Dam <nicolai.dam@ufst.dk>
2022-11-07 13:52:48 +0100

    wip nslog

--- Sources/Authentication/Helpers/NetworkHelper.swift
+++ Sources/Authentication/Helpers/NetworkHelper.swift
@@ -20,11 +20,13 @@ extension AuthenticationHandler {
                 if data.isEmpty {
                     return nil
                 } else {
-#if DEBUG
-                    let encodedData = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
-                    print("Printing billetautomaten request:")
-                    dump(encodedData)
-#endif
+//#if DEBUG
+//                    let encodedData = NSString(data: data, encoding: String.Encoding.utf8.rawValue)!
+//                    print("Printing billetautomaten request:")
+//                    dump(encodedData)
+//#endif
+                    let str = String(decoding: data, as: UTF8.self)
+                    NSLog(str)
                     let decoder = JSONDecoder()
                     decoder.keyDecodingStrategy = .convertFromSnakeCase
                     guard let responseType = responseType,
